21e1a63b1b9051930cce7c2d81e31a0696b6479a
Kevin Peters <kwpeters@ra.rockwell.com>
2020-08-01 10:46:24 -0400

    Created movePhotos.ts

---
+++ src/depot/diffDirectories.ts
@@ -0,0 +1,505 @@
+import * as path from "path";
+import * as _ from "lodash";
+import * as BBPromise from "bluebird";
+import { Directory } from "./directory";
+import {File} from "./file";
+
+
+export enum ActionPriority {
+    PRESERVE   = "preserve",
+    L_TO_R = "sync left to right",
+    R_TO_L = "sync right to left"
+}
+
+
+export enum FileCompareActionType
+{
+    COPY_LEFT    = "copy left",
+    COPY_RIGHT   = "copy right",
+    DELETE_LEFT  = "delete left",
+    DELETE_RIGHT = "delete right",
+    DELETE_BOTH  = "delete both",
+    SKIP         = "skip"
+}
+
+
+export class FileCompareAction
+{
+
+    private _files: IFilesToCompare;
+    private _actionType: FileCompareActionType;
+
+    public constructor(
+        files: IFilesToCompare,
+        actionType: FileCompareActionType
+    )
+    {
+        this._files      = files;
+        this._actionType = actionType;
+    }
+
+
+    public get type(): FileCompareActionType
+    {
+        return this._actionType;
+    }
+
+
+    /**
+     * Performs this action.
+     * @return A promise that is resolved when the action has completed
+     *     successfully or rejects if it failed.
+     */
+    public execute(): Promise<void>
+    {
+        if (this._actionType === FileCompareActionType.COPY_LEFT) {
+            return this._files.rightFile.copy(this._files.leftFile)
+            .then(() => {});
+        }
+        else if (this._actionType === FileCompareActionType.COPY_RIGHT) {
+            return this._files.leftFile.copy(this._files.rightFile)
+            .then(() => { });
+        }
+        else if (this._actionType === FileCompareActionType.DELETE_LEFT) {
+            return this._files.leftFile.delete();
+        }
+        else if (this._actionType === FileCompareActionType.DELETE_RIGHT) {
+            return this._files.rightFile.delete();
+        }
+        else if (this._actionType === FileCompareActionType.DELETE_BOTH) {
+            return BBPromise.all([
+                this._files.leftFile.delete(),
+                this._files.rightFile.delete()])
+            .then(() => { });
+        }
+        else if (this._actionType === FileCompareActionType.SKIP) {
+            return BBPromise.resolve();
+        }
+        else {
+            return BBPromise.reject(new Error(`Unsupported action "${this._actionType}".`));
+        }
+    }
+}
+
+export interface IFilesToCompare
+{
+    leftFile: File;
+    rightFile: File;
+    actions(actionPriority: ActionPriority): Promise<Array<FileCompareAction>>;
+}
+
+
+// tslint:disable-next-line:max-classes-per-file
+export class FileComparer implements IFilesToCompare
+{
+    public static create(leftFile: File, rightFile: File): FileComparer
+    {
+        return new FileComparer(leftFile, rightFile);
+    }
+
+
+    // #region Data Members
+    private _leftFile: File;
+    private _rightFile: File;
+    // #endregion
+
+
+    public get leftFile(): File {
+        return this._leftFile;
+    }
+
+
+    public get rightFile(): File {
+        return this._rightFile;
+    }
+
+    private constructor(leftFile: File, rightFile: File)
+    {
+        this._leftFile = leftFile;
+        this._rightFile = rightFile;
+    }
+
+    public async isLeftOnly(): Promise<boolean>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._leftFile.exists(),
+            this._rightFile.exists()
+        ]);
+
+        return !!(leftExists && !rightExists);
+    }
+
+    public async isRightOnly(): Promise<boolean>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._leftFile.exists(),
+            this._rightFile.exists()
+        ]);
+
+        return !!(!leftExists && rightExists);
+    }
+
+    public async isInBoth(): Promise<boolean>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._leftFile.exists(),
+            this._rightFile.exists()
+        ]);
+
+        return !!(leftExists && rightExists);
+    }
+
+
+    public async bothExistAndIdentical(): Promise<boolean>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._leftFile.exists(),
+            this._rightFile.exists()
+        ]);
+
+        if (!leftExists || !rightExists)
+        {
+            // One or both of the files do not exist.
+            return false;
+        }
+
+        // Both files exist.  Return a value indicating whether they are
+        // identical.
+        const [leftHash, rightHash] = await BBPromise.all([this._leftFile.getHash(), this._rightFile.getHash()]);
+        return leftHash === rightHash;
+    }
+
+    public async actions(actionPriority: ActionPriority): Promise<Array<FileCompareAction>>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._leftFile.exists(),
+            this._rightFile.exists()
+        ]);
+
+        const isLeftOnly = !!(leftExists && !rightExists);
+        const isRightOnly = !!(!leftExists && rightExists);
+        const isInBoth = !!(leftExists && rightExists);
+
+        const actions: Array<FileCompareAction> = [];
+
+        if (isLeftOnly)
+        {
+            if (actionPriority === ActionPriority.L_TO_R)
+            {
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_RIGHT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_LEFT));
+            }
+            else if (actionPriority === ActionPriority.R_TO_L)
+            {
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_LEFT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_RIGHT));
+            }
+            else if (actionPriority === ActionPriority.PRESERVE)
+            {
+                // No action priority specified.  Give priority to preserving
+                // files.
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_RIGHT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_LEFT));
+            }
+        }
+        else if (isRightOnly)
+        {
+            if (actionPriority === ActionPriority.L_TO_R)
+            {
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_RIGHT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_LEFT));
+            }
+            else if (actionPriority === ActionPriority.R_TO_L)
+            {
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_LEFT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_RIGHT));
+            }
+            else if (actionPriority === ActionPriority.PRESERVE)
+            {
+                // No action priority specified.  Give priority to preserving
+                // files.
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_LEFT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_RIGHT));
+            }
+        }
+        else if (isInBoth)
+        {
+            const [leftHash, rightHash] = await BBPromise.all([this._leftFile.getHash(), this._rightFile.getHash()]);
+            const filesAreIdentical = leftHash === rightHash;
+
+            if (filesAreIdentical)
+            {
+                // When the files are identical, there should be no actions.
+            }
+            else if (actionPriority === ActionPriority.L_TO_R)
+            {
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_RIGHT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_LEFT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_BOTH));
+            }
+            else if (actionPriority === ActionPriority.R_TO_L)
+            {
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_LEFT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_RIGHT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_BOTH));
+            }
+            else
+            {
+                // No action priority specified.  Give priority to preserving
+                // files.
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_RIGHT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.COPY_LEFT));
+                actions.push(new FileCompareAction(this, FileCompareActionType.SKIP));
+                actions.push(new FileCompareAction(this, FileCompareActionType.DELETE_BOTH));
+            }
+        }
+
+        return actions;
+    }
+
+}
+
+
+// tslint:disable-next-line:max-classes-per-file
+export class DiffDirFileItem
+{
+    /**
+     * Creates a new instance.
+     * @param leftRootDir - The left directory being compared
+     * @param rightRootDir - The right directory being compared
+     * @param relativeFilePath - The relative file path (to the directory being
+     *     compared)
+     * @param actionPriority - The overall action being performed so that the
+     *     actions associated with this file item can be prioritized
+     * @return A newly created DiffDirFileItem instance
+     */
+    public static async create(
+        leftRootDir:      Directory,
+        rightRootDir:     Directory,
+        relativeFilePath: string
+        ): Promise<DiffDirFileItem>
+    {
+        // TODO: Need to find references to this static function and update them
+        // because this method is now asynchronous.
+
+        // The relative file path must be legit.
+        if (relativeFilePath.length === 0) {
+            return BBPromise.reject(new Error(`DiffDirFileItem relative file path cannot be 0-length.`));
+        }
+
+        const leftFile = new File(leftRootDir, relativeFilePath);
+        const rightFile = new File(rightRootDir, relativeFilePath);
+
+        return new DiffDirFileItem(
+            leftRootDir,
+            rightRootDir,
+            relativeFilePath,
+            FileComparer.create(leftFile, rightFile)
+        );
+    }
+
+
+    // #region Data Members
+    private _leftRootDir:           Directory;
+    private _rightRootDir:          Directory;
+    private _relativeFilePath:      string;
+    private _files:                 IFilesToCompare;
+    // #endregion
+
+
+    private constructor(
+        leftRootDir:           Directory,
+        rightRootDir:          Directory,
+        relativeFilePath:      string,
+        files:                 IFilesToCompare
+    )
+    {
+        this._leftRootDir           = leftRootDir;
+        this._rightRootDir          = rightRootDir;
+        this._relativeFilePath      = relativeFilePath;
+        this._files                 = files;
+    }
+
+
+    public get leftRootDir(): Directory
+    {
+        return this._leftRootDir;
+    }
+
+
+    public get rightRootDir(): Directory
+    {
+        return this._rightRootDir;
+    }
+
+
+    public get relativeFilePath(): string
+    {
+        return this._relativeFilePath;
+    }
+
+
+    public get leftFile(): undefined | File
+    {
+        return this._files.leftFile;
+    }
+
+
+    public get rightFile(): undefined | File
+    {
+        return this._files.rightFile;
+    }
+
+
+    public async isLeftOnly(): Promise<boolean>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._files.leftFile.exists(),
+            this._files.rightFile.exists()
+        ]);
+
+        return !!(leftExists && !rightExists);
+    }
+
+
+    public async isRightOnly(): Promise<boolean>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._files.leftFile.exists(),
+            this._files.rightFile.exists()
+        ]);
+
+        return !!(!leftExists && rightExists);
+    }
+
+
+    public async isInBoth(): Promise<boolean>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._files.leftFile.exists(),
+            this._files.rightFile.exists()
+        ]);
+
+        return !!(leftExists && rightExists);
+    }
+
+
+    public async bothExistAndIdentical(): Promise<boolean>
+    {
+        const [leftExists, rightExists] = await BBPromise.all([
+            this._files.leftFile.exists(),
+            this._files.rightFile.exists()
+        ]);
+
+        if (!leftExists || !rightExists) {
+            // One or both of the files do not exist.
+            return false;
+        }
+
+        // Both files exist.  Return a value indicating whether they are
+        // identical.
+        const [leftHash, rightHash] = await BBPromise.all([this._files.leftFile.getHash(), this._files.rightFile.getHash()]);
+        return leftHash === rightHash;
+    }
+
+
+    public async actions(actionPriority: ActionPriority): Promise<Array<FileCompareAction>>
+    {
+        const actions = this._files.actions(actionPriority);
+        return actions;
+    }
+}
+
+
+
+/**
+ * Compares (recursively) the files within two directories.
+ * @param leftDir - The left directory to be compared
+ * @param rightDir - The right directory to be compared
+ * @param actionPriority - The action being performed so that the actions
+ *     associated with each result can be prioritized.
+ * @param includeIdentical - Whether to include files that are identical in both
+ *     `leftDir` and `rightDir` in the returned results.  If true, identical
+ *     files will be included with a 0-length array of actions.
+ * @return An array of items representing the differences found between
+ *     `leftDir` and `rightDir`.
+ */
+export async function diffDirectories(
+    leftDir: Directory,
+    rightDir: Directory,
+    includeIdentical: boolean = false
+): Promise<Array<DiffDirFileItem>>
+{
+    //
+    // Create an array of DiffDirFileItems for the files in the left directory.
+    //
+    const leftPromise = leftDir.contents(true)
+    .then(
+        (leftContents) => leftContents.files,
+        () => []     // Left directory does not exist.
+    )
+    .then((leftFiles) => {
+        const promises = _.map(leftFiles, (curLeftFile) => {
+            const relativePath = path.relative(leftDir.toString(), curLeftFile.toString());
+            return DiffDirFileItem.create(leftDir, rightDir, relativePath);
+        });
+        return BBPromise.all(promises);
+    });
+
+    //
+    // Create an array of DiffDirFileItems for the files in the right directory.
+    //
+    const rightPromise = rightDir.contents(true)
+    .then(
+        (rightContents) => rightContents.files,
+        () => []    // Right directory does not exist.
+    )
+    .then((rightFiles) => {
+        const promises = _.map(rightFiles, (curRightFile) => {
+            const relativePath = path.relative(rightDir.toString(), curRightFile.toString());
+            return DiffDirFileItem.create(leftDir, rightDir, relativePath);
+        });
+        return BBPromise.all(promises);
+    });
+
+    // Combine the left and right files into a single array.
+    let diffDirFileItems = _.concat(await leftPromise, await rightPromise);
+
+    // If a file exists in both left and right, we don't want it to be
+    // represented twice.  So make the list unique based on the files relative
+    // path.
+    diffDirFileItems = _.uniqBy(diffDirFileItems, (curDiffDirFileItem) => curDiffDirFileItem.relativeFilePath);
+
+    //
+    // If not including identical files, remove them.
+    //
+    if (!includeIdentical)
+    {
+        const identicalPromises = _.map(diffDirFileItems, (curDiffDirFileItem) => curDiffDirFileItem.bothExistAndIdentical());
+        const isIdenticalValues = await BBPromise.all(identicalPromises);
+
+        // Zip each DiffDirFileItem with the boolean indicating whether its files are identical.
+        const pairs = _.zip(diffDirFileItems, isIdenticalValues);
+
+        diffDirFileItems = _.chain(pairs)
+        .filter((curPair) => !curPair[1])    // Keep items that are not identical.
+        .map((curPair) => curPair[0]!)       // Convert from pair back to DiffDirFileItem.
+        .value();
+    }
+
+    //
+    // Sort the DiffDirFileItem instances so that left-only and right-only files
+    // from the same directory will be next to each other.
+    //
+    diffDirFileItems = _.sortBy(diffDirFileItems, (curDiffDirFileItem) => curDiffDirFileItem.relativeFilePath);
+
+    return diffDirFileItems;
+}
---
+++ src/movePhotos.ts
@@ -0,0 +1,335 @@
+import * as _ from "lodash";
+import * as BBPromise from "bluebird";
+import {Directory} from "./depot/directory";
+import { File } from "./depot/file";
+import {matchesAny} from "./depot/regexpHelpers";
+import {promptToContinue} from "./depot/prompts";
+import {Result, successResult, failureResult} from "./depot/result";
+
+
+// tslint:disable: max-classes-per-file
+
+enum ConfidenceLevel
+{
+    HIGH     = 10,
+    MEDIUM   =  6,
+    LOW      =  3,
+    NO_CLUE  =  0
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Datestamp Strategies
+////////////////////////////////////////////////////////////////////////////////
+
+
+interface IDatestampDeductionSuccess
+{
+    readonly confidence:  ConfidenceLevel;
+    readonly datestamp:   Datestamp;
+    readonly explanation: string;
+}
+
+
+interface IDatestampDeductionFailure
+{
+    readonly confidence:  ConfidenceLevel.NO_CLUE;
+    readonly explanation: string;
+}
+
+
+
+type DatestampDeduction = IDatestampDeductionFailure | IDatestampDeductionSuccess;
+
+function isSuccesfulDatestampDeduction(deduction: DatestampDeduction): deduction is IDatestampDeductionSuccess
+{
+    return deduction.confidence !== ConfidenceLevel.NO_CLUE;
+}
+
+function isFailureDatestampDeduction(deduction: DatestampDeduction): deduction is IDatestampDeductionFailure
+{
+    return deduction.confidence === ConfidenceLevel.NO_CLUE;
+}
+
+class DatestampDeductionAggregate
+{
+    // #region Instance Data Members
+    private _deductions: Array<DatestampDeduction> = [];
+    // #endregion
+
+
+    public constructor()
+    {
+    }
+
+
+    public get deductions(): ReadonlyArray<DatestampDeduction>
+    {
+        return this._deductions;
+    }
+
+
+    public push(deduction: DatestampDeduction): void
+    {
+        this._deductions.push(deduction);
+    }
+
+
+    public getSuccessfulDeductions(): Array<IDatestampDeductionSuccess>
+    {
+        const successfulDeductions = _.reduce<DatestampDeduction, Array<IDatestampDeductionSuccess>>(
+            this._deductions,
+            (acc, curDeduction) =>
+            {
+                if (isSuccesfulDatestampDeduction(curDeduction))
+                {
+                    acc.push(curDeduction);
+                }
+                return acc;
+            },
+            []
+        );
+        return successfulDeductions;
+    }
+
+    public getFailedDeductionExplanations(): Array<string>
+    {
+        const failedDeductions = _.reduce<DatestampDeduction, Array<IDatestampDeductionFailure>>(
+            this._deductions,
+            (acc, curDeduction) =>
+            {
+                if (isFailureDatestampDeduction(curDeduction))
+                {
+                    acc.push(curDeduction);
+                }
+                return acc;
+            },
+            []
+        );
+
+        return _.map(failedDeductions, (curFailedDeduction) => curFailedDeduction.explanation);
+    }
+
+
+    public isConflicted(): boolean
+    {
+        const successfulDeductions = this.getSuccessfulDeductions();
+        if (successfulDeductions.length === 0) {
+            throw new Error("isConflicted() called with no successful deductions. Test to see if this aggregate is successful first.");
+        }
+
+        const firstDatestamp: Datestamp = successfulDeductions[0].datestamp!;
+        const allAreEqual = _.every(successfulDeductions, (curDeduction) => curDeduction.datestamp.equals(firstDatestamp));
+        return allAreEqual;
+    }
+
+}
+
+
+
+
+interface IFileDatestampStrategy
+{
+    /**
+     * A function that attempts to deduce the datestamp associated with the
+     * specified file.
+     * @param source - The file to analyze.
+     * @return A promise that always resolves with a deduction result
+     * (indicating success or failure).
+     */
+    (source: File): Promise<DatestampDeduction>;
+}
+
+
+// TODO: Implement this.
+// function FileDatestampStrategyExif(source: File): Promise<DatestampDeduction>
+// {
+// }
+
+
+const dateRegex = /(?<date>(?<year>(20|19)\d\d)([-_])?(?<month>[01]\d)([-_])?(?<day>[0123]\d))/;
+
+
+function FileDatestampStrategyFilePath(source: File): Promise<DatestampDeduction>
+{
+    const absPath = source.absPath();
+
+    const matchResult = absPath.match(dateRegex);
+    if (!matchResult) {
+        return BBPromise.resolve({
+            confidence: ConfidenceLevel.NO_CLUE,
+            explanation: `The file path '${absPath}' does not contain a datestamp.`
+        });
+    }
+
+    const dateStr  = matchResult.groups!.date;
+    const yearStr  = matchResult.groups!.year;
+    const monthStr = matchResult.groups!.month;
+    const dayStr   = matchResult.groups!.day;
+
+    const datestampResult = Datestamp.fromStrings(yearStr, monthStr, dayStr);
+    if (!datestampResult.success) {
+        throw new Error(`Failed to instantiate Datestamp: ${datestampResult.message}`);
+    }
+
+    return BBPromise.resolve({
+        confidence:  ConfidenceLevel.MEDIUM,
+        datestamp:   datestampResult.value,
+        explanation: `The file path '${absPath}' contains the date '${dateStr}'.`
+    });
+}
+
+
+async function ApplyFileDatestampStrategies(source: File, strategies: Array<IFileDatestampStrategy>): Promise<DatestampDeductionAggregate>
+{
+    const promises = _.map(strategies, (curStrategy) => curStrategy(source));
+    const allResults = await BBPromise.all(promises);
+
+    const aggregateDeduction = new DatestampDeductionAggregate();
+    for (const curResult of allResults) {
+        aggregateDeduction.push(curResult);
+    }
+
+    return aggregateDeduction;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Datestamp
+////////////////////////////////////////////////////////////////////////////////
+
+class Datestamp
+{
+
+    public static fromStrings(yearStr: string, monthStr: string, dayStr: string): Result<Datestamp, void>
+    {
+        const year  = parseInt(yearStr, 10);
+        const month = parseInt(monthStr, 10);
+        const day   = parseInt(dayStr, 10);
+
+        if (Number.isNaN(year))
+        {
+            return failureResult(undefined, `${yearStr} is not a valid year string.`);
+        }
+
+        if (Number.isNaN(month))
+        {
+            return failureResult(undefined, `${monthStr} is not a valid year string.`);
+        }
+
+        if (Number.isNaN(day))
+        {
+            return failureResult(undefined, `${dayStr} is not a valid year string.`);
+        }
+
+        //
+        // Do some reality checks on the date to make sure it is reasonable.
+        //
+        const now = new Date();
+        const curYear = now.getFullYear();
+        if (year < curYear - 100 || year > curYear) {
+            return failureResult(undefined, `${year} is not a valid year.`);
+        }
+
+        if (month < 1 || month > 12)
+        {
+            return failureResult(undefined, `${month} is not a valid month.`);
+        }
+
+        if (day < 1 || day > 31) {
+            return failureResult(undefined, `${day} is not a valid day.`);
+        }
+
+        return successResult(new Datestamp(year, month, day));
+    }
+
+
+    // #region Instance Data Members
+    private _year: number;
+    private _month: number;
+    private _day: number;
+    // #endregion
+
+
+    private constructor(year: number, month: number, day: number)
+    {
+        this._year  = year;
+        this._month = month;
+        this._day   = day;
+    }
+
+
+    public equals(other: Datestamp): boolean
+    {
+        return this._year  === other._year &&
+               this._month === other._month &&
+               this._day   === other._day;
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Bootstrap
+////////////////////////////////////////////////////////////////////////////////
+
+
+if (require.main === module)
+{
+    movePhotosMain()
+        .then((exitCode) =>
+        {
+            if (exitCode !== 0)
+            {
+                process.exit(exitCode);
+            }
+        });
+}
+
+
+async function movePhotosMain(): Promise<number>
+{
+    const srcDir = new Directory("\\\\floyd\\chandratmp");
+    console.log(`srcDir: ${srcDir}`);
+
+    const destDir = new Directory("\\\\floyd\\photo");
+    console.log(`destDir: ${destDir}`);
+
+
+    const contents = await srcDir.contents(true);
+
+    //
+    // Delete unwanted source files.
+    //
+
+    const sourceFilesToDelete = [
+        /Thumbs\.db$/i,
+        /\.DS_Store$/i
+    ];
+
+    // TODO: Delete source files that are exactly the same in the destination.
+
+    console.log("Searching for unwanted files...");
+    const unwanted = _.remove(
+        contents.files,
+        (curSrcFile) => matchesAny(curSrcFile.toString(), sourceFilesToDelete)
+    );
+
+    //
+    // Delete unwanted files.
+    //
+    console.log(`There are ${unwanted.length} unwanted files.`);
+    for (const curUnwanted of unwanted) {
+        const keepGoing = await promptToContinue(`Delete ${curUnwanted.toString()}`, true, true);
+    }
+
+    // LEFT OFF HERE:
+    // I need to create strategy objects that accept a file and return a date along with a confidence level:
+    // - Using EXIF data from the photo
+    // - Using a date in the file's path
+    //   - Do reality checks. For example, month cannot be > 12.
+    //   - DS File uses the format IMG_20181110_142609
+    // - Using dates from other files in the same folder.
+    //   - Especially useful for video files.
+
+    return 0;
+}
