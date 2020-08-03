85c03ec80b731bc8126afe81d06ec18b95aaba28
Kevin Peters <kwpeters@ra.rockwell.com>
2020-08-03 09:10:33 -0400

    Enhanced windowsSpotlightImages.ts to ignore small files.

--- src/windowsSpotlightImages.ts
+++ src/windowsSpotlightImages.ts
@@ -2,6 +2,7 @@ import * as os from "os";
 import * as _ from "lodash";
 import {Directory} from "./depot/directory";
 import { File } from "./depot/file";
+import {mapAsync, filterAsync} from "./depot/promiseHelpers";
 
 
 if (require.main === module)
@@ -37,15 +38,21 @@ async function windowsSpotlightImagesMain(): Promise<number>
     const spotlightAssetsDir = new Directory(os.homedir(), "AppData", "Local", "Packages",
         "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy", "LocalState", "Assets"
     );
-    const contents = spotlightAssetsDir.contentsSync(false);
 
-    const promises = _.map(contents.files, async (curAssetFile) => {
-        const dstFile = new File(outDir, curAssetFile.baseName + ".jpg");
-        curAssetFile.copy(dstFile);
+    let assetFiles = spotlightAssetsDir.contentsSync(false).files;
+
+    // Keep only the files greater than a certain size.  This gets rid of icons
+    // that are also kept in this directory.
+    assetFiles = await filterAsync(assetFiles, async (curFile) => {
+        const stats = (await curFile.exists())!;
+        return stats.size > 200 * 1024;
     });
 
-    await Promise.all(promises);
+    const destFiles = await mapAsync(assetFiles, (curFile) => {
+        const dstFile = new File(outDir, curFile.baseName + ".jpg");
+        return curFile.copy(dstFile);
+    });
 
-    console.log(`Copied ${promises.length} files.`);
+    console.log(`Copied ${destFiles.length} files.`);
     return 0;
 }
