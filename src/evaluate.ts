7def2fe02e4c1872cb7af0b4c451717d1479ffd1
Kevin Peters <kwpeters@ra.rockwell.com>
2021-11-06 22:37:02 -0400

    Converted evaluate.ts to use yargs.

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -1,15 +1,33 @@
+import * as yargs from "yargs";
 import { evaluate } from "./depot/expression";
 import { succeeded } from "./depot/result";
 
-const expression = process.argv[2];
+
+const argv = yargs
+.usage("Evalutates an expression.")
+.help()
+.wrap(80)
+.demandCommand(
+    1,
+    1,
+    "Too few arguments.  Specify the expression (may need quotes).",
+    "Too many arguments.  Specify the expression as one argument (may need quotes)."
+)
+.argv;
+
+const expression = argv._[0];
 const result = evaluate(expression);
 
-if (succeeded(result)) {
+if (succeeded(result))
+{
+    // Form the output.
+    let answer = `${expression} = ${result.value.toString(true)}`;
 
+    // If the answer can be reduced, provide an additional " = ..." at the end
+    // of the output.
     const reduceResult = result.value.reduce();
-    let answer = `${expression} = ${result.value}`;
     if (reduceResult.wasReduced) {
-        answer += ` = ${reduceResult.reducedFraction}`;
+        answer += ` = ${reduceResult.reducedFraction.toString(false)}`;
     }
 
     console.log(answer);
