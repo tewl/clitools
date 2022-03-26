107ac04fef7b0b31358be9b343148dd29ee6471c
Kevin Peters <kwpeters@ra.rockwell.com>
2022-03-26 18:06:32 -0400

    Updated depot dependencies.

--- dev/depot/regexpHelpers.ts
+++ dev/depot/regexpHelpers.ts
@@ -1,4 +1,5 @@
 import * as _ from "lodash";
+import { failedResult, Result, succeededResult } from "./result";
 
 /**
  * Creates a new regular expression capable of detecting EOL.  Because regular
@@ -25,4 +26,85 @@ export function matchesAny(str: string, regexes: Array<RegExp>): boolean
 {
     const matchesAny = _.some(regexes, (curRegex) => str.match(curRegex));
     return matchesAny;
+}
+
+
+type RegExpFlag = "d" | "g" | "i" | "m" | "s" | "u" | "y";
+
+
+/**
+ * Creates a new RegExp without the specified flags.
+ * @param srcRegex - The source regex
+ * @param flagsToClear - The flag to clear
+ * @returns A new RegExp without the specified flag.
+ */
+export function clearFlags(srcRegex: RegExp, flagsToClear: Array<RegExpFlag>): RegExp
+{
+    let newFlags = srcRegex.flags;
+    flagsToClear.forEach((curFlag) =>
+    {
+        newFlags = newFlags.replace(curFlag, "");
+    });
+    const newRegex = new RegExp(srcRegex, newFlags);
+    return newRegex;
+}
+
+
+/**
+ * Creates a new RegExp with the specified flags set.
+ * @param srcRegex - The source regex
+ * @param flagsToSet - The flag to set
+ * @returns A new RegExp with the specified flag set
+ */
+export function setFlags(srcRegex: RegExp, flagsToSet: Array<RegExpFlag>): RegExp
+{
+    let newFlags = srcRegex.flags;
+    flagsToSet.forEach((curFlag) =>
+    {
+        newFlags = newFlags.replace(curFlag, "") + curFlag;
+    });
+    const newRegex = new RegExp(srcRegex, newFlags);
+    return newRegex;
+}
+
+
+/**
+ * Gets a RegExp used to match regular expression expressions.  Wrapped in
+ * this function to avoid problems with the instance's state.
+ * @returns A regular expression that matches regular expression expressions.
+ */
+function getRegExpStrRegExp(): RegExp
+{
+    const regexStrRegex = /^\/(?<pattern>.*)\/(?<flags>[dgimsuy]*)$/g;
+    return regexStrRegex;
+}
+
+
+/**
+ * Converts a string to a RegExp instance.
+ * @param str - The string to convert to a RegExp.  May be of the form
+ * "/<pattern>/<flags>" or "<pattern>".
+ * @returns A Result for the RegExp instance.  Upon failure, the error
+ * contains a descriptive error message.
+ */
+export function strToRegExp(str: string): Result<RegExp, string>
+{
+    const matches = getRegExpStrRegExp().exec(str);
+    try
+    {
+        if (matches)
+        {
+            const regex = new RegExp(matches.groups!.pattern, matches.groups!.flags);
+            return succeededResult(regex);
+        }
+        else
+        {
+            const regex = new RegExp(str);
+            return succeededResult(regex);
+        }
+    }
+    catch (err)
+    {
+        return failedResult((err as Error).message);
+    }
 }
