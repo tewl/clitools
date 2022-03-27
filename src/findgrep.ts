5adb60843beee75b2469abeda6cea8f170bd582a
Kevin Peters <kwpeters@ra.rockwell.com>
2022-03-27 13:05:53 -0400

    Added ability to just search file and directory names (text regex is optional).

--- src/findgrep.ts
+++ src/findgrep.ts
@@ -18,12 +18,7 @@ import { failed, Result, succeededResult } from "./depot/result";
 import { bindResult, boolToResult, mapWhileSuccessful } from "./depot/resultHelpers";
 import _ = require("lodash");
 import { pipe } from "./depot/pipe";
-
-
-const fileStyle = chalk.cyan;
-const fileMatchStyle = fileStyle.inverse;
-const lineStyle = chalk.yellow;
-const textMatchStyle  = chalk.inverse;
+import { boolToOption, isNone, isSome, mapSome, Option } from "./depot/option";
 
 
 if (require.main === module)
@@ -52,70 +47,23 @@ async function findGrepMain(): Promise<number>
         throw new Error(configResult.error);
     }
 
-    console.log(`path regex: ${configResult.value.pathRegex}`);
-    console.log(`path ignores: ${configResult.value.pathIgnores.join(", ")}`);
-    console.log(`text regex: ${configResult.value.textRegex}`);
-    console.log(`recurse:    ${configResult.value.recurse}`);
+    const config = configResult.value;
+
+    console.log(`path regex:   ${config.pathRegex}`);
+    console.log(`path ignores: ${config.pathIgnores.join(", ")}`);
+    console.log(`text regex:   ${config.textRegex}`);
+    console.log(`recurse:      ${config.recurse}`);
     console.log();
 
-    const pathRegex = configResult.value.pathRegex;
-    const textRegex = configResult.value.textRegex;
 
-    const cwd = new Directory(".");
-    await cwd.walk(async (fileOrDir): Promise<boolean> =>
+    if (isNone(config.textRegex))
     {
-        if (fileOrDir instanceof Directory)
-        {
-            // As a performance optimization, if the directory matches any of
-            // the pathIgnore patterns, do not recurse into it.  Otherwise,
-            // recurse into it as the user has specified.
-            return matchesAny(fileOrDir.toString(), configResult.value.pathIgnores) ?
-                false :
-                configResult.value.recurse;
-        }
-
-        // See if the file's path matches the path regular expression.  If not,
-        // we are done.
-        const [numPathMatches, highlightedPath] = highlightMatches(fileOrDir.toString(), pathRegex, fileMatchStyle);
-        if (numPathMatches === 0)
-        {
-            return false; // Return value does not matter when processing files.
-        }
-
-        // If the file's path should be ignored, we are done.
-        if (matchesAny(fileOrDir.toString(), configResult.value.pathIgnores))
-        {
-            return false; // Return value does not matter when processing files.
-        }
-
-        const fileText = fileStyle(highlightedPath);
-        let fileProducedOutput = false;
-
-        // Read the file line by line and output lines that match the text
-        // regular expression.
-
-        await fileOrDir.readLines((lineText, lineNum) =>
-        {
-            const [numMatches, highlightedText] = highlightMatches(lineText, textRegex, textMatchStyle);
-            if (numMatches > 0)
-            {
-                const lineNumText = lineStyle(`:${lineNum}`);
-                console.log(fileText + lineNumText + " - " + highlightedText);
-                fileProducedOutput = true;
-            }
-        });
-
-        if (fileProducedOutput)
-        {
-            // The current file produced output.  Leave a blank line at the of
-            // its output.
-            console.log();
-        }
-
-        return false; // Return value does not matter when processing files.
-    });
-
-    return 0;
+        return doFilesystemSearch(config);
+    }
+    else
+    {
+        return doTextSearch(config);
+    }
 }
 
 
@@ -126,7 +74,7 @@ interface IFindGrepConfig
 {
     recurse: boolean;
     pathRegex: RegExp;
-    textRegex: RegExp;
+    textRegex: Option<RegExp>;
     pathIgnores: Array<RegExp>;
 }
 
@@ -142,7 +90,14 @@ function getConfiguration(): Result<IFindGrepConfig, string>
     .usage(
         [
             "Finds text within files.",
-            `findgrep [options] "<pathRegex>" "<textRegex>"`
+            `findgrep [options] "<pathRegex>" ["<textRegex>"]`,
+            "",
+            "textRegex is optional.  If omitted, matching file names and directories will",
+            "be listed.  If included, matching files will be searched using the specified",
+            "regular expression.",
+            "",
+            'Regular expressions can be specified as "<pattern>" or "/<pattern>/<flags>".',
+            '  - Include the "g" flag to display multiple matches in a path or line'
         ].join(os.EOL)
     )
     .help()
@@ -176,15 +131,15 @@ function getConfiguration(): Result<IFindGrepConfig, string>
         return pathRegexResult;
     }
 
-    // Get the text regex positional argument.
-    const textRegexResult = pipe(
-        succeededResult(argv._[1]),
-        (r) => bindResult((v) => boolToResult(_.isString(v), v, "Text regex not specified."), r),
-        (r) => bindResult(strToRegExp, r)
+    // Get the optional text regex positional argument.
+    const textRegexOptResult = pipe(
+        argv._[1],
+        (str) => boolToOption(str, str),
+        (strOpt) => mapSome(strToRegExp, strOpt)
     );
-    if (failed(textRegexResult))
+    if (isSome(textRegexOptResult) && failed(textRegexOptResult.value))
     {
-        return textRegexResult;
+        return textRegexOptResult.value;
     }
 
     // Get the path ignore regexes from the --pathIgnore arguments.
@@ -201,6 +156,108 @@ function getConfiguration(): Result<IFindGrepConfig, string>
         recurse:     argv.recurse,
         pathRegex:   pathRegexResult.value,
         pathIgnores: pathIgnoresResult.value,
-        textRegex:   textRegexResult.value
+        textRegex:   mapSome((res) => res.value!, textRegexOptResult)
     });
+}
+
+
+async function doTextSearch(config: IFindGrepConfig): Promise<number>
+{
+    const styles = {
+        fileStyle:      chalk.cyan,
+        fileMatchStyle: chalk.inverse,
+        lineStyle:      chalk.yellow,
+        textMatchStyle: chalk.inverse
+    };
+
+    const cwd = new Directory(".");
+    await cwd.walk(async (fileOrDir): Promise<boolean> =>
+    {
+        const path = fileOrDir.toString();
+        const shouldBeIgnored = matchesAny(path, config.pathIgnores);
+        // If the current item has been explicitly ignored, do not recurse.
+        // Otherwise, recurse as the user has specified.
+        const shouldRecurse = shouldBeIgnored ? false : config.recurse;
+
+        if (shouldBeIgnored || (fileOrDir instanceof Directory))
+        {
+            return shouldRecurse;
+        }
+
+        const [numPathMatches, highlighedPath] = highlightMatches(
+            path, config.pathRegex, styles.fileMatchStyle
+        );
+
+        if (numPathMatches === 0)
+        {
+            return shouldRecurse;
+        }
+
+        const fileText = styles.fileStyle(highlighedPath);
+        let fileProducedOutput = false;
+
+        // Read the file line by line and output lines that match the text regex.
+        await fileOrDir.readLines((lineText, lineNum) =>
+        {
+            const [numMatches, highlightedText] = highlightMatches(
+                lineText,
+                config.textRegex.value!,
+                styles.textMatchStyle
+            );
+            if (numMatches > 0)
+            {
+                const lineNumText = styles.lineStyle(`:${lineNum}`);
+                console.log(fileText + lineNumText + " - " + highlightedText);
+                fileProducedOutput = true;
+            }
+        });
+
+        if (fileProducedOutput)
+        {
+            // The current file produced output.  Leave a blank line after it.
+            console.log();
+        }
+
+        return shouldRecurse;
+    });
+
+    return 0;
+}
+
+async function doFilesystemSearch(config: IFindGrepConfig): Promise<number>
+{
+    const styles = {
+        matchStyle: chalk.inverse
+    };
+
+    const cwd = new Directory(".");
+    await cwd.walk((fileOrDir): boolean =>
+    {
+        const path = fileOrDir.toString();
+        const shouldBeIgnored = matchesAny(path, config.pathIgnores);
+        // If the current item has been explicitly ignored, do not recurse.
+        // Otherwise, recurse as the user has specified.
+        const shouldRecurse = shouldBeIgnored ? false : config.recurse;
+
+        if (shouldBeIgnored)
+        {
+            return shouldRecurse;
+        }
+
+        const [numPathMatches, highlighted] = highlightMatches(
+            path,
+            config.pathRegex,
+            styles.matchStyle
+        );
+
+        if (numPathMatches > 0)
+        {
+            console.log(highlighted);
+        }
+
+        return shouldRecurse;
+
+    });
+
+    return 0;
 }
